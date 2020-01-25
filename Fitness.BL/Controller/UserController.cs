11d597cd8535b94c3402120aa7339a2dc64d95d4
Uliana Nikitina <56694279+Nikitina99@users.noreply.github.com>
2020-01-25 20:38:59 +0300

    Добавьте файлы проекта.

---
+++ Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Fitness.BL.Module;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Fitness.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь приложения.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"></param>
+        public UserController(string userName, string genderName, DateTime bridthDay, double weight, double height)
+        {
+            //Проверка
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, bridthDay, weight, height);
+
+        }
+        public UserController()  //конструктор
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if (formatter.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
+                //Чтоделать, если пользовател не прочитали?
+            }
+        }
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+            }
+        }
+
+        /// <summary>
+        /// Получить данные пользователя.
+        /// </summary>
+        /// <returns> Пользователь приложения. </returns>
+
+    }
+}
---
+++ Fitness.CMD/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Fitness.BL.Controller;
+
+namespace Fitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в приложение Fitness");
+            Console.WriteLine("Введите Ваше имя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваш пол");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите Вашу дату рождения");
+            var birthDay =DateTime.Parse( Console.ReadLine());//Переписать
+
+            Console.WriteLine("Введите Ваш вес");
+            var weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите Ваш рост");
+            var height =double.Parse( Console.ReadLine());
+
+            var userController = new UserController(
+                name,
+                gender,
+                birthDay,
+                weight,
+                height);
+            userController.Save();
+        }
+    }
+}
